fa3dece34631bd367de6f7103db6df959a1a1413
Joe Selvik <joe@thenewtricks.com>
2014-04-16 15:52:44 -0500

    working on deleting a session from scooby DB

--- FidoDoorOpener/TNTSignInViewController.m
+++ FidoDoorOpener/TNTSignInViewController.m
@@ -101,7 +101,7 @@
                                                              
                                                              if (!error && resp.statusCode == 201) {
                                                                  NSLog(@"Created a Session!");
-                                                                 NSLog(@"Code: %ld", (long)resp.statusCode);
+                                                                 NSLog(@"Resp: %@", resp);
                                                                  NSLog(@"Cookies[%lu]: %@", (unsigned long)[[sc.cookieJar cookies] count], [sc.cookieJar cookies]);
                                                                  
                                                                  dispatch_async(dispatch_get_main_queue(), ^{
