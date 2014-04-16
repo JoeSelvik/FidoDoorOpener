ce36fc7919191856c9a5e4a41b4baafc32e1e5bc
Joe Selvik <selvi041@umn.edu>
2014-04-15 23:50:08 -0500

    adding TNTSignInVC

---
+++ FidoDoorOpener/TNTSignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  TNTSignInViewController.h
+//  FidoDoorOpener
+//
+//  Created by Joe Selvik on 4/15/14.
+//  Copyright (c) 2014 Joe Selvik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNTSignInViewController : UIViewController
+
+@end
---
+++ FidoDoorOpener/TNTSignInViewController.m
@@ -0,0 +1,49 @@
+//
+//  TNTSignInViewController.m
+//  FidoDoorOpener
+//
+//  Created by Joe Selvik on 4/15/14.
+//  Copyright (c) 2014 Joe Selvik. All rights reserved.
+//
+
+#import "TNTSignInViewController.h"
+
+@interface TNTSignInViewController ()
+
+@end
+
+@implementation TNTSignInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
