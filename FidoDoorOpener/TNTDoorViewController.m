d33e63be3e3f66a4a98299dad78f490a24730e3e
Joe Selvik <joe@thenewtricks.com>
2014-05-02 23:55:41 -0500

    added DoorVC

---
+++ FidoDoorOpener/TNTDoorViewController.m
@@ -0,0 +1,52 @@
+//
+//  TNTDoorViewController.m
+//  FidoDoorOpener
+//
+//  Created by Joe Selvik on 5/2/14.
+//  Copyright (c) 2014 Joe Selvik. All rights reserved.
+//
+
+#import "TNTDoorViewController.h"
+
+@interface TNTDoorViewController ()
+- (IBAction)openDoorButtonPressed:(id)sender;
+
+@end
+
+@implementation TNTDoorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)openDoorButtonPressed:(id)sender {
+}
+@end
