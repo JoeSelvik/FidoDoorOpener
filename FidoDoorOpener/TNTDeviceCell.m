6eb01b9bb45b46c36f5a0e6e59bca562283e9552
Joe Selvik <joe@thenewtricks.com>
2014-05-08 01:34:01 -0500

    storyboard layout done

---
+++ FidoDoorOpener/TNTDeviceCell.m
@@ -0,0 +1,31 @@
+//
+//  TNTDeviceCell.m
+//  FidoDoorOpener
+//
+//  Created by Joe Selvik on 5/8/14.
+//  Copyright (c) 2014 Joe Selvik. All rights reserved.
+//
+
+#import "TNTDeviceCell.h"
+
+@implementation TNTDeviceCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ FidoDoorOpener/TNTHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  TNTHeaderView.h
+//  FidoDoorOpener
+//
+//  Created by Joe Selvik on 5/8/14.
+//  Copyright (c) 2014 Joe Selvik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNTHeaderView : UICollectionReusableView
+
+@end
